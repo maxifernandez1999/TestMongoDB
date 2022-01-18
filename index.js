75860acfa53ece760d1bed3961f3d3bad9bbec16
maxifernandez1999 <fmaximiliano443@gmail.com>
2022-01-18 17:28:26 -0300

    new entrypoint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const User = require("./models/user");
@@ -7,10 +7,12 @@ const PORT = 3000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //PARA SUBIR A HEROKU HAY QUE AGREGAR LAS ENV EN EL REMOTO
 const URI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ce12s.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-main().then(res => console.log("BD connect")).catch(err => console.log(err));
+main()
+  .then((res) => console.log("BD connect"))
+  .catch((err) => console.log(err));
 // 'mongodb://localhost:27017/test'
 async function main() {
   await mongoose.connect(URI);
@@ -19,29 +21,54 @@ app.listen(process.env.PORT || PORT, () => {
   console.log(`The server started on port ${PORT} !!!!!!`);
 });
 
-app.get("/users",async (req, res) => {
+app.get("/users", async (res) => {
   try {
     const arrayUsers = await User.find();
-    res.send(
-      `${arrayUsers}`
-    );
+    res.send(`${arrayUsers}`);
   } catch (error) {
     console.log(error);
   }
-  
 });
-app.post("/add",async (req, res) => {
+app.get(`/users/:id`, async (req, res) => {
   try {
-    const user = new User({
-      email: "miriam@gmail.com",
-      password: "123456"
-    })
-    await user.save();
-    res.send(
-      `User saved`
-    );
+    let id = req.params.id;
+    const user = await User.findById(id).exec();
+    res.send(`${user}`);
   } catch (error) {
     console.log(error);
   }
-  
+});
+app.post("/add", async (req, res) => {
+  try {
+    const dataUser = req.body;
+    const user = new User({
+      email: dataUser.email,
+      password: dataUser.password,
+    });
+    await user.save();
+    res.send(`User saved`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/update", async (req, res) => {
+  try {
+    const dataUser = req.body;
+    await User.updateOne(
+      { _id: dataUser.id },
+      { password: dataUser.password }
+    );
+    res.send(`User updated`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.delete("/delete", async (req, res) => {
+  try {
+    const dataUser = req.body;
+    await User.findByIdAndDelete(dataUser.id);
+    res.send(`User deleted`);
+  } catch (error) {
+    console.log(error);
+  }
 });
